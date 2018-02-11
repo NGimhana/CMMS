47e85efd155baff3f3391139b4dc9158be2f60c8
Nadeeshan Gimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2018-02-11 09:21:10 +0530

    Maintenance Sector Create and View Added

---
+++ database/migrations/2018_02_11_021223_create_buildings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buildings');
+    }
+}
