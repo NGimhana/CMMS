53e9428437459b8a09efcc6a2767a92556c8f6c0
NGimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2018-04-18 19:42:09 +0530

    Calendar Events Updated
    Signed-off-by: NGimhana <gimhanadesilva.15@cse.mrt.ac.lk>

--- public/js/app.js
+++ public/js/app.js
@@ -119699,17 +119699,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
-//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
     data: function data() {
         return {
-
-            allCalendarEvents: [],
-            jobs: [],
-            selectedJob: ''
+            jobs: []
         };
     },
 
@@ -119721,104 +119716,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
     methods: {
-        showCalendar: function showCalendar() {
-
-            $('#calendar').fullCalendar({
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'listDay,listWeek,month'
-                },
-
-                // customize the button names,
-                // otherwise they'd all just say "list"
-                views: {
-                    listDay: { buttonText: 'list day' },
-                    listWeek: { buttonText: 'list week' }
-                },
-
-                defaultView: 'listWeek',
-                defaultDate: '2018-03-12',
-                navLinks: true, // can click day/week names to navigate views
-                editable: true,
-                eventLimit: true, // allow "more" link when too many events
-                // events: [
-                //     {
-                //         title: 'All Day Event',
-                //         start: '2018-03-01'
-                //     },
-                //     {
-                //         title: 'Long Event',
-                //         start: '2018-03-07',
-                //         end: '2018-03-10'
-                //     },
-                //     {
-                //         id: 999,
-                //         title: 'Repeating Event',
-                //         start: '2018-03-09T16:00:00'
-                //     },
-                //     {
-                //         id: 999,
-                //         title: 'Repeating Event',
-                //         start: '2018-03-16T16:00:00'
-                //     },
-                //     {
-                //         title: 'Conference',
-                //         start: '2018-03-11',
-                //         end: '2018-03-13'
-                //     },
-                //     {
-                //         title: 'Meeting',
-                //         start: '2018-03-12T10:30:00',
-                //         end: '2018-03-12T12:30:00'
-                //     },
-                //     {
-                //         title: 'Lunch',
-                //         start: '2018-03-12T12:00:00'
-                //     },
-                //     {
-                //         title: 'Meeting',
-                //         start: '2018-03-12T14:30:00'
-                //     },
-                //     {
-                //         title: 'Happy Hour',
-                //         start: '2018-03-12T17:30:00'
-                //     },
-                //     {
-                //         title: 'Dinner',
-                //         start: '2018-03-12T20:00:00'
-                //     },
-                //     {
-                //         title: 'Birthday Party',
-                //         start: '2018-03-13T07:00:00'
-                //     },
-                //     {
-                //         title: 'Click for Google',
-                //         url: 'http://google.com/',
-                //         start: '2018-03-28'
-                //     }
-                // ]
-                events: this.allCalendarEvents
-            });
-        },
-
-        loadAllJob: function loadAllJob() {
-            // this.$http.get('http://localhost:8000/api/calendars').then(response=>{
-            //     let obj = JSON.parse(JSON.stringify(response.body));
-            //     let immediatejobs = [];
-            //
-            //     obj.forEach(function(value){
-            //         immediatejobs.push(value.job_id);
-            //     });
-            //
-            //     this.jobID = immediatejobs;
-            //     console.log(this.jobID);
-            // },response=>{
-            //
-            // });
-
-
-        },
 
         fetchData: function fetchData() {
             var _this = this;
@@ -119853,7 +119750,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                     },
 
                     defaultView: 'listWeek',
-                    defaultDate: '2018-03-12',
+                    defaultDate: new Date().toLocaleDateString(),
                     navLinks: true, // can click day/week names to navigate views
                     editable: true,
                     eventLimit: true, // allow "more" link when too many events
@@ -119863,14 +119760,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
                 console.log(calenadarEvents);
             });
-        },
-
-        getImmediateJobDetails: function getImmediateJobDetails(id) {
-            var _this2 = this;
-
-            this.$http.get('http://localhost:8000/api/job/' + id).then(function (response) {
-                _this2.selectedJob = JSON.parse(JSON.stringify(response.body));
-            }, function (response) {});
         }
 
     }
--- resources/assets/js/components/CalendarComponent.vue
+++ resources/assets/js/components/CalendarComponent.vue
@@ -1,9 +1,7 @@
 <template>
     <div style="width: 700px; height: 400px;">
         <div id="calendar"></div>
-
     </div>
-
 </template>
 
 <script>
@@ -12,10 +10,7 @@ export default {
 
     data() {
         return {
-
-            allCalendarEvents: [],
             jobs: [],
-            selectedJob:'',
         }
     },
     computed:{
@@ -29,105 +24,6 @@ export default {
 
 
     methods: {
-        showCalendar:function () {
-
-            $('#calendar').fullCalendar({
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'listDay,listWeek,month'
-                },
-
-                // customize the button names,
-                // otherwise they'd all just say "list"
-                views: {
-                    listDay: { buttonText: 'list day' },
-                    listWeek: { buttonText: 'list week' }
-                },
-
-                defaultView: 'listWeek',
-                defaultDate: '2018-03-12',
-                navLinks: true, // can click day/week names to navigate views
-                editable: true,
-                eventLimit: true, // allow "more" link when too many events
-                // events: [
-                //     {
-                //         title: 'All Day Event',
-                //         start: '2018-03-01'
-                //     },
-                //     {
-                //         title: 'Long Event',
-                //         start: '2018-03-07',
-                //         end: '2018-03-10'
-                //     },
-                //     {
-                //         id: 999,
-                //         title: 'Repeating Event',
-                //         start: '2018-03-09T16:00:00'
-                //     },
-                //     {
-                //         id: 999,
-                //         title: 'Repeating Event',
-                //         start: '2018-03-16T16:00:00'
-                //     },
-                //     {
-                //         title: 'Conference',
-                //         start: '2018-03-11',
-                //         end: '2018-03-13'
-                //     },
-                //     {
-                //         title: 'Meeting',
-                //         start: '2018-03-12T10:30:00',
-                //         end: '2018-03-12T12:30:00'
-                //     },
-                //     {
-                //         title: 'Lunch',
-                //         start: '2018-03-12T12:00:00'
-                //     },
-                //     {
-                //         title: 'Meeting',
-                //         start: '2018-03-12T14:30:00'
-                //     },
-                //     {
-                //         title: 'Happy Hour',
-                //         start: '2018-03-12T17:30:00'
-                //     },
-                //     {
-                //         title: 'Dinner',
-                //         start: '2018-03-12T20:00:00'
-                //     },
-                //     {
-                //         title: 'Birthday Party',
-                //         start: '2018-03-13T07:00:00'
-                //     },
-                //     {
-                //         title: 'Click for Google',
-                //         url: 'http://google.com/',
-                //         start: '2018-03-28'
-                //     }
-                // ]
-                events: this.allCalendarEvents,
-            });
-
-        },
-
-        loadAllJob: function () {
-            // this.$http.get('http://localhost:8000/api/calendars').then(response=>{
-            //     let obj = JSON.parse(JSON.stringify(response.body));
-            //     let immediatejobs = [];
-            //
-            //     obj.forEach(function(value){
-            //         immediatejobs.push(value.job_id);
-            //     });
-            //
-            //     this.jobID = immediatejobs;
-            //     console.log(this.jobID);
-            // },response=>{
-            //
-            // });
-
-
-        },
 
         fetchData: function () {
             this.$http.get('http://localhost:8000/api/job').then(response => {
@@ -162,7 +58,7 @@ export default {
                     },
 
                     defaultView: 'listWeek',
-                    defaultDate: '2018-03-12',
+                    defaultDate: new Date().toLocaleDateString(),
                     navLinks: true, // can click day/week names to navigate views
                     editable: true,
                     eventLimit: true, // allow "more" link when too many events
@@ -170,20 +66,10 @@ export default {
 
                 });
 
-
                 console.log(calenadarEvents);
 
             });
         },
-
-        getImmediateJobDetails:function (id) {
-            this.$http.get('http://localhost:8000/api/job/'+id).then(response => {
-                this.selectedJob = JSON.parse(JSON.stringify(response.body));
-            },response => {
-
-            });
-        },
-
 
     }
 };
