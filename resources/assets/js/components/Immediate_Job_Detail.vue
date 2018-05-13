f5b092e99ba89856d844c19c994206fed8ab43ca
NGimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2018-05-13 15:08:25 +0530

    Specific Job Bug Fixed

--- resources/assets/js/components/Immediate_Job_Detail.vue
+++ resources/assets/js/components/Immediate_Job_Detail.vue
@@ -1,18 +1,38 @@
 <template>
 
     <div class="ui segment">
-        <h4 class="ui header">Job Task </h4>
+        <h3 class="ui header">Job Task : <span>{{this.jobDetail.id}}</span></h3>
+
+        <a class="ui green image label">
+            Status
+            <div v-bind:class="[this.isCompleted ? 'green detail' : 'orange detail']">
+                {{this.status}}
+            </div>
+            <div v-bind:class="[this.isOverdue ? 'red detail' : 'red detail']">
+                {{this.overDueStatus}}
+            </div>
+
+        </a>
+
+        <br>
+        <br>
+        <div style="padding-left: 7px; padding-bottom: 3px">
+            <div v-show="!this.isCompleted" class="ui  green button " v-on:click="completeJobTask()"><i class="checkmark icon"></i>COMPLETE</div>
+        </div>
+
+        <br>
+        <br>
         <form class="ui form">
 
             <div class="ui grid">
-                <div class="four wide column">
+                <div class="six wide column">
 
-                    <div class="inline fields">
+                    <div class="inline two fields">
                         <div class="field">
                             <label>Description</label>
                         </div>
                         <div class="field">
-                            <label style="color:red">{{this.jobDetail.description}}</label>
+                            <p style="color:red"> {{this.jobDetail.description}} </p>
                         </div>
                     </div>
 
@@ -21,7 +41,43 @@
                             <label>Asset</label>
                         </div>
                         <div class="field">
-                            <label style="color:red">{{this.asset.description}}</label>
+                            <p style="color:red">{{this.asset.description}}</p>
+                        </div>
+                    </div>
+
+                    <div class="inline two fields">
+                        <div class="field">
+                            <label>Building</label>
+                        </div>
+                        <div class="field">
+                            <p style="color:red">{{this.building.description}}</p>
+                        </div>
+                    </div>
+
+                    <div class="inline two fields">
+                        <div class="field">
+                            <label>Sector</label>
+                        </div>
+                        <div class="field">
+                            <p style="color:red">{{this.sector.description}}</p>
+                        </div>
+                    </div>
+
+                    <div class="inline two fields">
+                        <div class="field">
+                            <label>Scheduled End Date</label>
+                        </div>
+                        <div class="field">
+                            <p style="color:red">{{this.jobDetail.Scheduled_End_Date}}</p>
+                        </div>
+                    </div>
+
+                    <div class="inline two fields">
+                        <div class="field">
+                            <label>End Date</label>
+                        </div>
+                        <div class="field">
+                            <p style="color:red">{{this.jobDetail.Ended_Date}}</p>
                         </div>
                     </div>
 
@@ -34,7 +90,7 @@
                             <label>Created By</label>
                         </div>
                         <div class="field">
-                            <label style="color:red">{{this.createduser.name}}</label>
+                            <p style="color:red">{{this.createduser.name}}</p>
                         </div>
                     </div>
 
@@ -43,30 +99,16 @@
                             <label>Assigned To</label>
                         </div>
                         <div class="field">
-                            <label style="color:red">{{this.assigneduser.name}}</label>
+                            <p style="color:red">{{this.assigneduser.name}}</p>
                         </div>
                     </div>
 
                 </div>
 
-                <div class="four wide column">
-
-                    <div class="inline fields">
-                        <div class="field">
-                            <label>Building</label>
-                        </div>
-                        <div class="field">
-                            <label style="color:red">{{this.building.description}}</label>
-                        </div>
-                    </div>
-
-                    <div class="inline fields">
-                        <div class="field">
-                            <label>Sector</label>
-                        </div>
-                        <div class="field">
-                            <label style="color:red">{{this.sector.description}}</label>
-                        </div>
+                <div class="six wide column">
+                    <div class="content">
+                        <iframe id="mapFrame" width="100%" height="450" frameborder="0" style="border:0"
+                                v-bind:src=" this.source "></iframe>
                     </div>
 
                 </div>
@@ -74,8 +116,6 @@
 
             </div>
 
-
-            <button class="ui positive button" type="submit">Complete</button>
 
         </form>
 
@@ -107,6 +147,20 @@
                 asset:'',
                 building:'',
                 sector:'',
+
+                status: '',
+                overDueStatus: '',
+
+                today: '',
+                dd: '',
+                mm: '',
+                yyyy: '',
+
+                source:'',
+
+                isCompleted: true,
+                isOverdue: '',
+
             };
         },
 
@@ -126,6 +180,9 @@
             this.getCreatedUser(this.jobDetail.created_user_id);
             this.getAssignedUser(this.jobDetail.Assigned_Person_id);
             this.fetchData(this.jobDetail.asset_id);
+            this.checkStatus();
+            //Create Map
+            this.createmap(this.building.description);
         },
 
 
@@ -147,29 +204,11 @@
 
                 });
             },
-            getAsset:function(id){
-                this.$http.get("http://localhost:8000/api/asset/"+id).then(response=>{
-                    this.asset =response.data;
-                },response=>{
 
-                })
-            },
-            getBuilding:function (id) {
-                this.$http.get("http://localhost:8000/api/sector/building/"+id).then(response=>{
-                    this.building = response.body;
-                    console.log(this.building);
-                },response=>{
-                    console.log(this.response.body);
-                });
-            },
-
-            getSector:function (id) {
-                this.$http.get("http://localhost:8000/api/sector/"+id).then(response=>{
-                    this.sector = response.body;
-                    console.log(this.sector);
-                },response=>{
-                    console.log(this.response);
-                });
+            //Map of the Selected Place of Job Task
+            createmap(jobPlace) {
+                this.source = "https://www.google.com/maps/embed/v1/search?q="+jobPlace+"&key=AIzaSyBOGCtQ84xGgjZaBiZ48ruA9B3g7AiT0Xg";
+                document.getElementById('mapFrame').setAttribute("src", this.source);
             },
 
             fetchData:function(id){
@@ -195,7 +234,84 @@
                 },response=>{
 
                 })
-            }
+            },
+
+            getToday: function () {
+                this.today = new Date();
+                this.dd = this.today.getDate();
+                this.mm = this.today.getMonth() + 1;
+                this.yyyy = this.today.getFullYear();
+
+                if (this.dd < 10) {
+                    this.dd = '0' + this.dd;
+                }
+
+                if (this.mm < 10) {
+                    this.mm = '0' + this.mm;
+                }
+
+                this.today = this.yyyy + "-" + this.mm + '-' + this.dd;
+            },
+            checkStatus: function () {
+                if (this.jobDetail.Ended_Date !== null) {
+
+                    if (this.jobDetail.Scheduled_End_Date >= this.jobDetail.Ended_Date) {
+
+                        this.status = "COMPLETED";
+                        this.overDueStatus = "";
+                        this.isCompleted = true;
+                        this.isOverdue = false;
+
+                    } else {
+                        this.status = "COMPLETED";
+                        this.overDueStatus = "OVERDUE";
+                        this.isCompleted = true;
+                        this.isOverdue = false;
+                    }
+                } else {
+
+                    this.getToday();
+
+                    if (this.jobDetail.Scheduled_End_Date > this.today) {
+
+                        this.status = "ON GOING";
+                        this.overDueStatus = "";
+                        this.isCompleted = false;
+                        this.isOverdue = false;
+
+                    } else {
+
+                        this.status = "ON GOING";
+                        this.overDueStatus = "OVERDUE";
+                        this.isCompleted = false;
+                        this.isOverdue = true;
+                    }
+                }
+            },
+            completeJobTask: function () {
+                this.getToday();
+                this.$http.put('http://localhost:8000/api/job/'+this.jobDetail.id ,
+                    {
+
+                        type:this.jobDetail.type,
+                        asset_id:this.jobDetail.asset_id,
+                        description:this.jobDetail.description,
+                        priority:this.jobDetail.priority,
+                        scheduled_end_date:this.jobDetail.Scheduled_End_Date,
+                        assigned_person:this.jobDetail.Assigned_Person_id,
+                        starteddate:this.jobDetail.Started_Date,
+                        created_user_id:this.jobDetail.created_user_id,
+                        Ended_Date:this.today
+                    }
+
+                ).then(response => {
+                    console.log(response.body);
+                    location.reload();
+                    //this.$router.go("http://localhost:8000/job");
+                },response => {
+
+                });
+            },
         }
     }
 </script>
