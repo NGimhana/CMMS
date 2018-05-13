4bdec5d55100e670cb043325455f58577c2a3fac
NGimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2018-05-13 22:47:10 +0530

    Calendar Event Added

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Computerised Maintenance Management System</title>   
+    <title>COTENCE | Computerised Maintenance Management System</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
